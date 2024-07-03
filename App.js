4c8e10e7e9da397388b14b0451bc922cd1f42053
Baiquan Fan <baiquan.fan@gmail.com>
2024-07-03 21:47:25 +0300

    Styling React Native apps.

--- App.js
+++ App.js
@@ -1,29 +1,21 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.appContainer}>
       <View>
-        <Text style={styles.dummyText}>Hello World!</Text>
+        <TextInput placeholder='Your course goal!' />
+        <Button title='Add goal!' />
       </View>
-      <Text style={styles.dummyText}>Open up App.js to start working on your app!</Text>
-      <Button title="Tap me!" />
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  dummyText: {
-    margin: 16,
-    padding: 16,
-    borderWidth: 2,
-    borderColor: 'red',
-  },
+  appContainer: {
+    padding: 50,
+  }
 });
