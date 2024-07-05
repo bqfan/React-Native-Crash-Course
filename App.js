c8c9f512cd29d9fa524410f22d7789d43d3903bd
Baiquan Fan <baiquan.fan@gmail.com>
2024-07-05 10:32:19 +0300

    Splitting up components, Working with props & components.

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem';
 
 export default function App() {
   const [enteredGoalText, setEnteredGoalText] = useState('');
@@ -27,11 +28,7 @@ export default function App() {
       </View>
       <View style={styles.goalsContainer}>
         <FlatList data={courseGoals} renderItem={(itemData) => {
-          return (
-            <View style={styles.goalItem}>
-              <Text style={styles.goalText}>{itemData.item}</Text>
-            </View>
-          );
+          return <GoalItem text={itemData.item} />;
         }} alwaysBounceVertical={false} />
       </View>
     </View>
@@ -66,15 +63,4 @@ const styles = StyleSheet.create({
   goalsContainer: {
     flex: 5
   },
-
-  goalItem: {
-    margin: 8,
-    padding: 8,
-    borderRadius: 6,
-    backgroundColor: '#5e0acc'
-  },
-
-  goalText: {
-    color: 'white'
-  }
 });
